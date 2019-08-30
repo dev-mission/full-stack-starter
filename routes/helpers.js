7e7b89d713c889e58a1476412c653c20cffde338
Francis Li <mail@francisli.com>
2019-08-29 17:20:44 -0700

    [dev-mission/full-stack-starter#11] Helper to set Link header pagination in response

--- routes/helpers.js
+++ routes/helpers.js
@@ -1,11 +1,50 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const _ = require('lodash');
 const fs = require('fs-extra');
 const inflection = require('inflection');
+const _ = require('lodash');
 const mime = require('mime-types');
 const path = require('path');
+const querystring = require('querystring');
+
+
+module.exports.setPaginationHeaders = function(req, res, page, pages, total) {
+  const baseURL = `${process.env.BASE_URL}${req.baseUrl}${req.path}?`;
+  const query = _.clone(req.query);
+  let link = '';
+  page = parseInt(page);
+  if (page < (pages - 1)) {
+    query.page = page + 1;
+    link += `<${baseURL}${querystring.stringify(query)}>; rel="next"`;
+  }
+  if (page < (pages - 2)) {
+    if (link.length > 0) {
+      link += ',';
+    }
+    query.page = pages;
+    link += `<${baseURL}${querystring.stringify(query)}>; rel="last"`;
+  }
+  if (page > 2) {
+    if (link.length > 0) {
+      link += ',';
+    }
+    query.page = 1;
+    link += `<${baseURL}${querystring.stringify(query)}>; rel="first"`;
+  }
+  if (page > 1) {
+    if (link.length > 0) {
+      link += ',';
+    }
+    query.page = page - 1;
+    link += `<${baseURL}${querystring.stringify(query)}>; rel="prev"`;
+  }
+  const headers = {
+    'X-Total-Count': total,
+    'Link': link
+  };
+  res.set(headers);
+}
 
 module.exports.register = function(res, errors) {
   res.locals.inflection = inflection;
