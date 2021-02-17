6d213a23256ebbba90d2c801e470e1ec2715195f
Emanuel Castro <emanuel@minerva.kgi.edu>
2021-02-17 09:04:44 -0800

    Added a description in about me

--- client/src/AboutMe.js
+++ client/src/AboutMe.js
@@ -6,7 +6,7 @@ function AboutMe() {
     <div id="about-me">
       <img className="img-fluid mb-3" src={me} alt="My Name" />
       <h1>César Castro</h1>
-      <p style="text-align:center;">Minerva Schools at KGI | Class of 2022</p>
+      <p>Minerva Schools at KGI | Class of 2022</p>
     </div>
   );
 }
