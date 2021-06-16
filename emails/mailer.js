4374ee1e34ab33e3745c4cd1161a52e68a0afc4d
Francis Li <mail@francisli.com>
2021-06-15 17:50:56 -0700

    Add SMTP control variable

--- emails/mailer.js
+++ emails/mailer.js
@@ -2,19 +2,16 @@
 
 const Email = require('email-templates');
 
-if (process.env.MAILGUN_SMTP_SERVER) {
-  process.env.SMTP_HOST = process.env.MAILGUN_SMTP_SERVER;
-  process.env.SMTP_PORT = process.env.MAILGUN_SMTP_PORT;
-  process.env.SMTP_USERNAME = process.env.MAILGUN_SMTP_LOGIN;
-  process.env.SMTP_PASSWORD = process.env.MAILGUN_SMTP_PASSWORD;
-}
+let transport;
 
-const email = new Email({
-  message: {
-    from: `${process.env.REACT_APP_SITE_TITLE} <${process.env.SMTP_FROM_EMAIL_ADDRESS}>`
-  },
-  send: true,
-  transport: {
+if (process.env.SMTP_ENABLED === 'true') {
+  if (process.env.MAILGUN_SMTP_SERVER) {
+    process.env.SMTP_HOST = process.env.MAILGUN_SMTP_SERVER;
+    process.env.SMTP_PORT = process.env.MAILGUN_SMTP_PORT;
+    process.env.SMTP_USERNAME = process.env.MAILGUN_SMTP_LOGIN;
+    process.env.SMTP_PASSWORD = process.env.MAILGUN_SMTP_PASSWORD;
+  }
+  transport = {
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     secure: process.env.SMTP_PORT === 465,
@@ -22,7 +19,19 @@ const email = new Email({
       user: process.env.SMTP_USERNAME,
       pass: process.env.SMTP_PASSWORD
     }
+  };
+} else {
+  transport = {
+    jsonTransport: true
+  };
+}
+
+const email = new Email({
+  message: {
+    from: `${process.env.REACT_APP_SITE_TITLE} <${process.env.SMTP_FROM_EMAIL_ADDRESS}>`
   },
+  send: true,
+  transport,
   views: {
     options: {
       extension: 'ejs'
