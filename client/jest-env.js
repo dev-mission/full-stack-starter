bc7faf71f86818910959b9dd3bd29b02e0132ada
Francis Li <mail@francisli.com>
2023-01-31 18:50:07 -0800

    Add reference link for new custom config

--- client/jest-env.js
+++ client/jest-env.js
@@ -1,5 +1,11 @@
 const Environment = require('jest-environment-jsdom');
 
+/**
+ * Custom JEST configuration to address issue as seen/described in:
+ *
+ * https://stackoverflow.com/a/57713960
+ *
+ */
 module.exports = class CustomTestEnvironment extends Environment {
   async setup() {
     await super.setup();
