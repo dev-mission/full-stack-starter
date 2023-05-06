0fc72003f52c34a60df1a01fee6cae3501400c01
Francis Li <mail@francisli.com>
2023-05-06 14:42:07 -0700

    Add support for AWS SES email sending

--- server/emails/mailer.js
+++ server/emails/mailer.js
@@ -1,13 +1,36 @@
+const crypto = require('crypto');
 const Email = require('email-templates');
 
 let transport;
 
-if (process.env.SMTP_ENABLED === 'true') {
+if (process.env.NODE_ENV === 'test') {
+  // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+  transport = require('nodemailer-mock').createTransport(transport);
+} else if (process.env.SMTP_ENABLED === 'true') {
   if (process.env.MAILGUN_SMTP_SERVER) {
     process.env.SMTP_HOST = process.env.MAILGUN_SMTP_SERVER;
     process.env.SMTP_PORT = process.env.MAILGUN_SMTP_PORT;
     process.env.SMTP_USERNAME = process.env.MAILGUN_SMTP_LOGIN;
     process.env.SMTP_PASSWORD = process.env.MAILGUN_SMTP_PASSWORD;
+  } else if (process.env.AWS_SES_REGION) {
+    process.env.SMTP_HOST = `email-smtp.${process.env.AWS_SES_REGION}.amazonaws.com`;
+    process.env.SMTP_PORT = 587;
+    process.env.SMTP_USERNAME = process.env.AWS_SES_ACCESS_KEY_ID;
+    // convert secret access key to SMTP password
+    // based on pseudocode at: https://docs.aws.amazon.com/ses/latest/dg/smtp-credentials.html#smtp-credentials-convert
+    const date = '11111111';
+    const service = 'ses';
+    const terminal = 'aws4_request';
+    const message = 'SendRawEmail';
+    const version = 4;
+    let signature;
+    signature = crypto.createHmac('sha256', `AWS4${process.env.AWS_SES_SECRET_ACCESS_KEY}`, { encoding: 'utf8' }).update(date).digest();
+    signature = crypto.createHmac('sha256', signature).update(process.env.AWS_SES_REGION).digest();
+    signature = crypto.createHmac('sha256', signature).update(service).digest();
+    signature = crypto.createHmac('sha256', signature).update(terminal).digest();
+    signature = crypto.createHmac('sha256', signature).update(message).digest();
+    signature = Buffer.concat([new Uint8Array([version]), signature]);
+    process.env.SMTP_PASSWORD = signature.toString('base64');
   }
   transport = {
     host: process.env.SMTP_HOST,
@@ -22,10 +45,6 @@ if (process.env.SMTP_ENABLED === 'true') {
   transport = {
     jsonTransport: true,
   };
-}
-if (process.env.NODE_ENV === 'test') {
-  // eslint-disable-next-line global-require, import/no-extraneous-dependencies
-  transport = require('nodemailer-mock').createTransport(transport);
 }
 
 const email = new Email({
