faa7dd7231c0f70cd6f09f8ff07ba14e5fe7dfc0
Francis Li <mail@francisli.com>
2023-05-25 11:42:18 -0700

    Fix test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders home', () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   const linkElement = screen.getByText(/Full Stack Starter/i);
   expect(linkElement).toBeInTheDocument();
 });
