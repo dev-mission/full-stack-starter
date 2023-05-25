b52c73faee8fa284c999a50b6570841addf49090
Francis Li <mail@francisli.com>
2023-05-25 14:55:05 -0700

    Fix test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,13 +1,22 @@
 import { render, screen } from '@testing-library/react';
+import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
+import { defaultValue, StaticContextProvider } from './StaticContext';
+
+export const TestApp = (
+  <StaticContextProvider value={defaultValue}>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </StaticContextProvider>
+);
 
 test('renders home', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+  render(TestApp);
   const linkElement = screen.getByText(/Full Stack Starter/i);
   expect(linkElement).toBeInTheDocument();
 });
