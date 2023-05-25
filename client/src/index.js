0a0a6dc8914307068869fc90d0275d75507a1e4c
Francis Li <mail@francisli.com>
2023-05-24 17:55:10 -0700

    Switch to hydrateRoot in production

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (process.env.NODE_ENV === 'development') {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  hydrateRoot(container, <App />);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
